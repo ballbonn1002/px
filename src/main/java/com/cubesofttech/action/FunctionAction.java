75def15415c8c7b42b135dea0f4f01f6dec68032
Korakot Hongthong <kung.cpe@gmail.com>
2022-06-27 14:56:27 +0700

    update function actual working

--- src/main/java/com/cubesofttech/action/FunctionAction.java
+++ src/main/java/com/cubesofttech/action/FunctionAction.java
@@ -1,45 +1,67 @@
 package com.cubesofttech.action;
 
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.cubesofttech.dao.FunctionDAO;
+import com.cubesofttech.model.WorkHours;
 import com.opensymphony.xwork2.ActionSupport;
 
-import com.cubesofttech.dao.UserDAO;
-
-import com.cubesofttech.dao.Employee_typeDAO;
-import com.cubesofttech.dao.UserSalaryDAO;
-import com.cubesofttech.model.UserSalary;
-
 public class FunctionAction extends ActionSupport {
+	private static final Logger log = Logger.getLogger(PaymentTypeAction.class);
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	public FunctionDAO funtionDAO;
 
 	HttpServletRequest request = ServletActionContext.getRequest();
 	HttpServletResponse response = ServletActionContext.getResponse();
-
-	@Autowired
-	private UserDAO userDAO;
-	private UserSalaryDAO userSalaryDAO;
-
-	public String salaryAction() {
+	
+	
+	public String getWorkingList() {
 		try {
-			List<Map<String, Object>> cubesoftUsers = userDAO.Query_Userlist();
-			request.setAttribute("cubesoftUsers", cubesoftUsers);
+			String userId = request.getParameter("userId") == null ? "test.data1" : request.getParameter("userId") ;
+			String startDate = request.getParameter("startDate");
+			String endDate = request.getParameter("endDate");
 			
-			//List<UserSalary> cubesoftUserSalaries = userSalaryDAO.findAllUserSalary();
-			//request.setAttribute("cubesoftUserSalaries", cubesoftUserSalaries);
-
+			//Set default if startDate,endDate null value
+			Date date = new Date();
+			LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();		
+			LocalDate lastDayOfMonth = localDate.with(TemporalAdjusters.lastDayOfMonth());
+			LocalDate firstDayOfMonth = localDate.with(TemporalAdjusters.firstDayOfMonth());
+			
+			if(startDate == null) {
+				startDate = firstDayOfMonth.toString();
+			}
+			if(endDate == null) {
+				endDate = lastDayOfMonth.toString();
+			}
+						
+			List<Map<String, Object>> workingList = funtionDAO.findWorkingList(userId, startDate, endDate);	//List for display on table detail
+			List<Map<String, Object>> workingSummary = funtionDAO.findWorkingSummary(userId, startDate, endDate); //Summary (record 0) : count_working,actual_working,absent,sum_hours
+					
+			request.setAttribute("WorkingList", workingList);
+			request.setAttribute("WorkingSummary", workingSummary);
+			
 			return SUCCESS;
+
 		} catch (Exception e) {
-			e.printStackTrace();
+
 			return ERROR;
 		}
-
 	}
-
 }
--- src/main/java/com/cubesofttech/dao/FunctionDAO.java
+++ src/main/java/com/cubesofttech/dao/FunctionDAO.java
@@ -1,5 +1,6 @@
 package com.cubesofttech.dao;
 import com.cubesofttech.model.User;
+import com.cubesofttech.model.UserSalary;
 
 import java.util.List;
 import java.util.Map;
@@ -10,9 +11,9 @@ import com.cubesofttech.model.WorkHours;
 import com.cubesofttech.model.LeaveType;
 
 public interface FunctionDAO {
-
+  
 	public List<Map<String, Object>> findWorkingList(String userId,String startDate,String endDate) throws Exception;
 	
 	public List<Map<String, Object>> findWorkingSummary(String userId,String startDate,String endDate) throws Exception;
-	
+		
 }
