557f1adda35802a72e52122df3ea7318b1b1876c
woraphonw <91590235+woraphonw@users.noreply.github.com>
2022-06-27 10:06:05 +0700

    Create CalcService

---
+++ src/main/java/com/cubesofttech/service/CalcService.java
@@ -0,0 +1,19 @@
+package com.cubesofttech.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cubesofttech.dao.*;
+import com.cubesofttech.model.*;
+
+@Service
+public class CalcService {
+	//@Autowired Phase
+	
+	
+	//Code in here
+	
+
+}
