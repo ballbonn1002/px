b5e6d01a3df82341b34e332fce66b27797b1083c
suphanut wandee <77426667+Filmsuphanut@users.noreply.github.com>
2022-07-05 09:28:39 +0700

    update_merge

--- src/main/java/com/cubesofttech/dao/Payment_groupDAOImpl.java
+++ src/main/java/com/cubesofttech/dao/Payment_groupDAOImpl.java
@@ -1,5 +1,8 @@
 package com.cubesofttech.dao;
 
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -99,11 +102,34 @@ public class Payment_groupDAOImpl implements Payment_groupDAO{
 		List<Map<String, Object>> query_listMap = null;
 		Session session =  this.sessionFactory.getCurrentSession(); 
 		try {
-			String sql = "SELECT payment.user_id , payment_detail.payment_type_id , payment_group.payment_date ,payment_detail.amount FROM payment JOIN payment_group ON payment.user_id = '"+userId+"' AND payment_group.payment_group_id = payment.payment_group_id AND payment_group.payment_date LIKE '"+Year+"%' JOIN payment_detail ON payment.payment_id = payment_detail.payment_id"; 
+			String sql = "SELECT payment.user_id , payment_detail.payment_type_id , EXTRACT(MONTH FROM payment_group.payment_date) AS payment_month , EXTRACT(YEAR FROM payment_group.payment_date) AS payment_year,payment_detail.amount FROM payment JOIN payment_group ON payment.user_id = '"+userId+"' AND payment_group.payment_group_id = payment.payment_group_id AND payment_group.payment_date LIKE '"+Year+"%' JOIN payment_detail ON payment.payment_id = payment_detail.payment_id";
 			SQLQuery query = session.createSQLQuery(sql);
 			query.setResultTransformer(AliasToEntityMapResultTransformer.INSTANCE);
 			query_listMap = query.list();
-			Log.debug(sql);
+			Log.debug(query_listMap);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return query_listMap;
+	}
+	
+	@Override
+	public List<Map<String, Object>> findBonusByMultipleYear(String userId,List<String> listOfYear) throws Exception {
+		List<Map<String, Object>> query_listMap = null;
+		Session session =  this.sessionFactory.getCurrentSession(); 
+		try {
+			String sql = " "; 
+	        for (int i = 0 ; i < listOfYear.size() ; i++) {
+	        	sql += "SELECT payment.user_id , payment_detail.payment_type_id , EXTRACT(YEAR FROM payment_group.payment_date) AS payment_year , SUM(payment_detail.amount ) AS amount FROM payment JOIN payment_group ON payment.user_id = '"+userId+"' AND payment_group.payment_group_id = payment.payment_group_id AND payment_group.payment_date LIKE '"+listOfYear.get(i)+"%' JOIN payment_detail ON payment.payment_id = payment_detail.payment_id  GROUP BY payment_type_id ";
+	        	if (i < listOfYear.size()-1 ) {
+	        		sql += "UNION ";
+	        	}
+	        }
+			SQLQuery query = session.createSQLQuery(sql);
+			query.setResultTransformer(AliasToEntityMapResultTransformer.INSTANCE);
+			query_listMap = query.list();
+			Log.debug(query_listMap);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
